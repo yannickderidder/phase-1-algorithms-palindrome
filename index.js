9c02a2fd281e372ab34b8d2890e293dc57b1a8aa
stanley <stanley.njuguna@student.moringaschool.com>
2024-01-11 06:45:19 +0300

    coplete assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  //  Write your algorithm here
 
+  const wordArray = word.split("")
+  const reversedWordArray = wordArray.reverse()
+  const reversedWord = reversedWordArray.join("")
+  return reversedWord;
+
+
+   if (word === reversedWord){
+     return true
+   } else {
+     return false
+}
+}
 /* 
   Add your pseudocode here
-*/
+
+  
 
 /*
   Add written explanation of your solution here
+
+ Reverse the word.
+ Check if the reversed word is equal to the original word.
+ Return true if they are equal, false otherwise.
 */
 
 // You can run `node index.js` to view these console logs
